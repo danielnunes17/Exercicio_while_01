b7936d528fcf9531c0dc1f3ead797d6fcd7c3bfe
Daniel Stephanio Nunes <dti584@localiza.com>
2023-02-28 11:14:36 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.*/
+
+public class Program
+{
+    public static void Main(string[] arg)
+    {
+        Console.WriteLine("Digite sua senha: ");
+        int senha = int.Parse(Console.ReadLine());
+
+        while (senha != 2002)
+        {
+            Console.WriteLine("Senha incorreta");
+            break;
+        }
+        if (senha == 2002)
+            Console.WriteLine("Acesso Permitido");
+    }
+}
